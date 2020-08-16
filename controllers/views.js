9f091760d2dcae703fece4f0e045e9d5e657cf87
kwanxhen <kwanxhenng@gmail.com>
2020-08-16 11:19:55 +0800

    checkpoint, working on profile page profile card

--- controllers/views.js
+++ controllers/views.js
@@ -5,6 +5,8 @@ router.get("/", function(req, res){
   res.sendFile('index.html', { root: "views" });
 });
 
-
+router.get("/profile", function(req, res) {
+  res.sendFile("profile.html", { root: "views" });
+});
 
 module.exports = router
